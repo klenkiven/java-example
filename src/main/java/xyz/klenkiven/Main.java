2f09ee6c96944b35bf3ce60293e53831853ad1d9
zzj <2361553935>
2022-10-13 11:07:10 +0800

    sysy

--- src/main/java/xyz/klenkiven/Main.java
+++ src/main/java/xyz/klenkiven/Main.java
@@ -1,19 +1,27 @@
 package xyz.klenkiven;
 
-import com.sun.source.tree.Tree;
 import xyz.klenkiven.collection.BPlusTreeMap.BPlusTreeMap;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        BPlusTreeMap<Integer, Integer> map = new BPlusTreeMap<>(4000);
-        // TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        BPlusTreeMap<Integer, Integer> map = new BPlusTreeMap<>(1000);
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
         List<Integer> data = new ArrayList<>();
-        for (int i = 0; i < 10000000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             data.add(i);
         }
+        Collections.shuffle(data);
         long startTime = System.currentTimeMillis();
+        data.forEach(item -> map.put(item, item - 1));
         System.out.println(System.currentTimeMillis() - startTime);
+        Collections.shuffle(data);
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 1000000 - 20; i++) {
+            map.remove(data.get(i));
+        }
+        System.out.println(System.currentTimeMillis() - startTime);
+        System.out.println(map.getList().toString());
     }
 }
